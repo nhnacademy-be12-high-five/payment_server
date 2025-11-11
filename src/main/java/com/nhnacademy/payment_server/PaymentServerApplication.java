3c43e09d7145ff60f05676972675178b6ede22cd
sungw0o <sungwoo515@gmail.com>
2025-11-11 23:26:17 +0900

    :sparkles: feat: 유레카 클라이언트 설정 및 서비스 등록

---
+++ src/main/java/com/nhnacademy/payment_server/PaymentServerApplication.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.payment_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServerApplication.class, args);
+    }
+
+}
