8614750f47cfbf1d950b10e1bde05bbf322f11a8
songdohyeok <songdh801@naver.com>
2025-12-01 17:28:37 +0900

    📝 docs: 주석 수정

--- src/main/java/com/nhnacademy/payment_server/dto/request/PointTransactionRequest.java
+++ src/main/java/com/nhnacademy/payment_server/dto/request/PointTransactionRequest.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PointTransactionRequest {
-    // 포인트 적립 사용 환불 요청을 위해 복사한 DTO
+    // 포인트 사용과 환불 요청을 위해 복사해온 DTO
     private Long memberId;
     private Long amount;
     private Long orderId;
