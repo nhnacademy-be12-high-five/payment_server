5e79ec26d8eb9438596941fab4fb44a11921e107
songdohyeok <songdh801@naver.com>
2025-12-16 09:20:58 +0900

    ✨ feat: 관리자 매출 통계 기능 에러처리 추가 [#13]

--- src/main/java/com/nhnacademy/payment_server/controller/AdminPaymentController.java
+++ src/main/java/com/nhnacademy/payment_server/controller/AdminPaymentController.java
@@ -3,13 +3,18 @@ package com.nhnacademy.payment_server.controller;
 import com.nhnacademy.payment_server.docs.AdminPaymentSwagger;
 import com.nhnacademy.payment_server.dto.response.DailySalesResponse;
 import com.nhnacademy.payment_server.dto.response.PaymentStatsResponse;
+import com.nhnacademy.payment_server.exception.BusinessException;
+import com.nhnacademy.payment_server.exception.ErrorCode;
 import com.nhnacademy.payment_server.service.PaymentStatService;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,13 +32,26 @@ public class AdminPaymentController implements AdminPaymentSwagger {
 
     @GetMapping("/daily")
     @Override
-    public ResponseEntity<List<DailySalesResponse>> getDailyStats(LocalDate startDate, LocalDate endDate) {
-        // 날짜 파라미터 없으면 최근 7일
-        if (endDate == null) endDate = LocalDate.now();
-        if (startDate == null) startDate = endDate.minusDays(7);
+    public ResponseEntity<List<DailySalesResponse>> getDailyStats(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        if (endDate == null) {
+            endDate = LocalDate.now();
+        }
+
+        if (startDate == null) {
+            startDate = endDate.minusDays(6);  // 날짜 파라미터 없으면 최근 7일
+        }
+
+        if (startDate.isAfter(endDate)) {
+            throw new BusinessException(ErrorCode.INVALID_DATE_RANGE);
+        }
 
         return ResponseEntity.ok(
-                paymentStatService.getDailyStats(startDate.atStartOfDay(), endDate.atTime(23, 59, 59))
+                paymentStatService.getDailyStats(startDate.atStartOfDay(), endDate.atTime(LocalTime.MAX))
         );
     }
 }
